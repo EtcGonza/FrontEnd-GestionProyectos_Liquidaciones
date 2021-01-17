3c418697ee846db43c9b06d8957eaaa948ec1d8e
EtcGonza <xetchegaraygonzalo@gmail.com>
2021-01-17 14:24:13 -0300

    Muevo page login a carpeta pages.

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MensagesAlertService } from '../services/mensages-alert.service';
+import { MensagesAlertService } from '../../services/mensages-alert.service';
 
 @Component({
   selector: 'app-login',
